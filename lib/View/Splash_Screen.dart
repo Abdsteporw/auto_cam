002b3d4bd7de11fbd2588d2f50ee765ac1a2fef8
Abdsteporw <106707711+Abdsteporw@users.noreply.github.com>
2023-04-06 21:01:12 +0400

    Initial commit

---
+++ lib/View/Splash_Screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Splash_Screen extends StatelessWidget {
+  const Splash_Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:auto_cam/View/Splash_Screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+
+void main() {
+  runApp(GetMaterialApp(home: Splash_Screen()));
+
+
+}
+
